2c1eb4a8f7f613da7bf3f82b6c68ee718c7b0392
PatricioArosD <parosd21@gmail.com>
2026-02-24 22:29:49 -0300

    agregar logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,27 @@ import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { LanguageContext } from '../context/LanguageContext';
 
+// 1. Importamos el logo desde la carpeta assets
+// Asegúrate de que la extensión (.png, .svg, .jpg) sea la correcta
+import logo from '../assets/logo-hymnia.tech.png';
+
 const NavBar = () => {
   const { language, toggleLanguage, t } = useContext(LanguageContext);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-midnight sticky-top shadow-sm py-3">
       <div className="container">
-        <NavLink className="navbar-brand fs-3 text-caramel" to="/">hymnia.tech</NavLink>
+        
+        {/* 2. Reemplazamos el texto puro por la imagen del logo */}
+        <NavLink className="navbar-brand d-flex align-items-center" to="/">
+          <img 
+            src={logo} 
+            alt="hymnia.tech logo" 
+            height="45" /* Puedes ajustar esta altura (en píxeles) para que se vea proporcionado */
+            className="d-inline-block align-text-top"
+          />
+          {<span className="ms-2 fs-4 text-caramel serif-font d-none d-sm-block">hymnia.tech</span>}
+        </NavLink>
         
         <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
